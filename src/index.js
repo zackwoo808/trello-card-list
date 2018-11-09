8ea62ee5beb3440dc692b7ced162649c8718cdd0
Zack Woo <zwoo18@bu.edu>
2018-11-08 21:06:09 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Board = ({ name, cards }) => (
+	<div className="board">
+		<div className="outer-border">
+			<div className="inner-border">
+				<BoardTitle name={name} />
+				<CardList cards={cards} />
+				<BoardFooter />
+			</div>
+		</div>
+	</div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const BoardFooter = () => (
+	<div className="footer-container">
+		Add a card...
+	</div>
+);
+
+const BoardTitle = ({ name }) => (
+	<div className="title-container">
+	   <span className="title">{name}</span>
+	   <span className="dots">...</span>
+	</div>
+);
+
+const CardList = ({ cards }) => (
+	<div className="card-list">
+		<table>
+		    <tbody>
+		        {cards.map(card => (
+		            <CardListItem key={card.id} card={card} />
+		        ))}
+		    </tbody>
+		</table>
+	</div>
+);
+
+CardList.propTypes = {
+	cards: PropTypes.array
+};
+
+const CardListItem = ({ card }) => (
+	<div className="card">
+	  <div className="card-container">
+	    <p>{card.text}</p> 
+	  </div>
+	</div>
+);
+
+// data
+
+const Boards = [
+	{
+		name: "Phone Features",
+		cards: [
+			{
+				text: "Subwoofer"
+			},
+			{
+				text: "Non-porous, washable"
+			},
+			{
+				text: "Wings"
+			},
+			{
+				text: "Beveled Bezel"
+			},
+			{
+				text: "Bezeled Bevel"
+			},
+			{
+				text: "Seedless"
+			}
+		]
+	}
+]
+
+ReactDOM.render(
+	<Board
+		name={Boards[0].name}
+		cards={Boards[0].cards}
+	/>
+	, document.getElementById('root')
+);
